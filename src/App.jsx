da8d04dc3a31893ff215f17e1bbeaf83d5e0deaf
Nikhil Kholiya <nikhilkholiya08@gmail.com>
2025-11-28 18:28:44 +0530

    get back to normal

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import './App.css'
-import Counter from './components/Counter/Counter'
-
 
 function App() {
   return (
     <>
       <h1 style={{ textAlign: 'center' }}>ReactMiniExercises</h1>
-      <Counter/>
+      
     </>
   )
 }
