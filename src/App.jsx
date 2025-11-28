6fe751ef41abf711cd95125559f63cce0ce38d57
Nikhil Kholiya <nikhilkholiya08@gmail.com>
2025-11-28 18:34:35 +0530

    render users list using map function complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import UserList from './components/UserList/UserList'
 
 function App() {
   return (
     <>
       <h1 style={{ textAlign: 'center' }}>ReactMiniExercises</h1>
-      
+      <UserList/>
     </>
   )
 }
--- src/components/UserList/UserList.jsx
+++ src/components/UserList/UserList.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
 
 const UserList = () => {
+      const styles = {
+         Container:{
+            border: '1px solid #333',
+            display: 'inlineBlock',
+            padding: '10px',
+            width: '350px',
+            margin:'10px'
+          },
+          li: {
+              listStyle:'none'
+          }
+    }  
+    const users = ['Nikhil', 'Harsh', 'Inderjeet', 'Vinay', 'Jatin']
   return (
-    <div>UserList</div>
+      <div style={styles.Container}>
+          <h3>UserList</h3>
+          <div>
+              {users.map((id) => (
+                  <li key={id} style={styles.li}>{id}</li>
+              ))}
+          </div>
+      </div>
   )
 }
 
