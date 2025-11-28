012fcd0f13d98bbc850c54a9d96d652f84b2da53
Nikhil Kholiya <nikhilkholiya08@gmail.com>
2025-11-28 18:19:52 +0530

    get back to normal version

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import './App.css'
-import UserCard from './components/UserCard/UserCard'
+
 
 function App() {
   return (
     <>
       <h1 style={{ textAlign: 'center' }}>ReactMiniExercises</h1>
-      <UserCard name='Nikhil' age='20' profession='Softwaredeveloper'/>
-      <UserCard name='Harsh' age='21' profession='Data Analyst'/>
-      <UserCard name='InderJeet' age='21' profession='Electronic Engineer'/>
+     
     </>
   )
 }
