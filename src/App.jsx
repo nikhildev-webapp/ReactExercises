ab1c34737edb1d4d16582b3d16cf3c71755a989c
Nikhil Kholiya <nikhilkholiya08@gmail.com>
2025-11-28 18:17:38 +0530

    exercise-first-complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import './App.css'
+import UserCard from './components/UserCard/UserCard'
 
 function App() {
   return (
     <>
-      <h1 style={{textAlign:'center'}}>ReactMiniExercises</h1>
+      <h1 style={{ textAlign: 'center' }}>ReactMiniExercises</h1>
+      <UserCard name='Nikhil' age='20' profession='Softwaredeveloper'/>
+      <UserCard name='Harsh' age='21' profession='Data Analyst'/>
+      <UserCard name='InderJeet' age='21' profession='Electronic Engineer'/>
     </>
   )
 }
--- src/components/UserCard/UserCard.jsx
+++ src/components/UserCard/UserCard.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
 
-const UserCard = () => {
+const UserCard = ({name,age,profession}) => {
+    const styles = {
+         Container:{
+            border: '1px solid #333',
+            display: 'inlineBlock',
+            padding: '10px',
+            width: '350px',
+            margin:'10px'
+        }
+    }
   return (
-    <div>UserCard</div>
+      <div style={styles.Container}>
+         <h1>Hello:{name}</h1>
+         <h3>Age:{age}</h3>
+         <h3>Profession:{profession}</h3>
+      </div>
   )
 }
 
