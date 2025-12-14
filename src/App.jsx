22e18e4588f127e2d1465adcc16e36c572cdcff5
Nikhil Kholiya <nikhilkholiya08@gmail.com>
2025-12-14 17:26:30 +0530

    get back to normal

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import './App.css'
-import TodoList from './components/TodoList/TodoList'
 
 function App() {
   return (
     <>
       <h1 style={{ textAlign: 'center' }}>ReactMiniExercises</h1>
       {/* continue with day 5 exercise */}
-      <TodoList/>
+    
     </>
   )
 }
