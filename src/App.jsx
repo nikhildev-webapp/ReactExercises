0c4af6ced07e81a6c6b45a3f20615ec1a049f14c
Nikhil Kholiya <nikhilkholiya08@gmail.com>
2025-12-15 16:54:46 +0530

    simple form exercise complete-add the conditional rendering to complete exercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import './App.css'
+import SimpleForm from './components/SimpleForm/SimpleForm'
 
 function App() {
   return (
     <>
       <h1 style={{ textAlign: 'center' }}>ReactMiniExercises</h1>
-      {/* continue with day 5 exercise */}
-    
+      <SimpleForm/>   
     </>
   )
 }
--- src/components/SimpleForm/SimpleForm.jsx
+++ src/components/SimpleForm/SimpleForm.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useState } from "react";
 
-const `SimpleForm` = () => {
+const SimpleForm = () => {
+  const [name, setName] = useState("");
+  const[isNameSaved,setIsNameSaved]=useState(false)
+  
+    const saveName = (e) => {
+        e.preventDefault();
+        setIsNameSaved(true)
+ }
+ 
   return (
-    <div>`SimpleForm`</div>
-  )
-}
+    <>
+      {/* Take name input and show 'Hello, [name]' */}
+        {
+              isNameSaved && name ? (
+                  <p>Hello,{name}</p>
+              ) : (
+                 <form onSubmit={saveName}>
+          <label htmlFor="name">Enter your name:</label>
+          <input
+            type="text"
+            id="name"
+            value={name}
+            placeholder="Enter your name"
+            onChange={(e) => {
+              setName(e.target.value);
+              setIsNameSaved(false); 
+            }}
+          />
+          <button type="submit" disabled={!name}>Save Name</button>
+          {!isNameSaved && <p>Hello, Please Enter name</p>}
+        </form>     
+            )
+        }
+    </>
+  );
+};
 
-export default `SimpleForm`
+export default SimpleForm;
