6f1dbe06635e60d86e5d1753f72c18dfe658e061
Nikhil Kholiya <nikhilkholiya08@gmail.com>
2025-12-16 16:42:05 +0530

    get back to normal everythinfg to next exercise

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,7 @@ function App() {
   return (
     <>
       <h1 style={{ textAlign: 'center' }}>ReactMiniExercises</h1>
-      <ProfileCard avatar="👨‍💻" name='Nikhil' role='Developer'/>
-      <ProfileCard avatar="👨‍💻" name='Harsh' role='Data Scientist'/>
-      <ProfileCard avatar="👨‍💻" name='Inderjeet' role='Electronic Engineer'/>
+      
     </>
   )
 }
