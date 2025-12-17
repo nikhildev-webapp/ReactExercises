c021099a52daae90895a2ce6d4f01a72dad800c6
Nikhil Kholiya <nikhilkholiya08@gmail.com>
2025-12-17 16:43:15 +0530

    Star rating exercise completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
 import ImageGallerySwitcher from './components/ImageGallerySwitcher/ImageGallerySwitcher'
+import StarRating from './components/StarRating.jsx/StarRating'
 
 function App() {
   return (
     <>
       <h1 style={{ textAlign: 'center' }}>ReactMiniExercises</h1>
-          
+          <StarRating/>
     </>
   )
 }
--- src/components/StarRating.jsx/StarRating.jsx
+++ src/components/StarRating.jsx/StarRating.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const StarRating = () => {
+    const [rating, setRating] = useState(0);
   return (
-    <div>StarRating</div>
+      <>
+          <h1>Start Rating</h1>
+          <p>Click the Star and Give Rating...</p>
+          {[1, 2, 3, 4, 5].map((star) => (
+              <span
+                  key='star'
+                  onClick={() => setRating(star)}
+                  style={{
+                      cursor: 'pointer',
+                      color: star <= rating ? 'gold' : 'grey',
+                      fontSize:'2rem'
+                  }}
+              >
+               ⭐   
+              </span>
+          ))}
+          <p>Your Rating:{rating}/5</p>
+      </>
   )
 }
 
